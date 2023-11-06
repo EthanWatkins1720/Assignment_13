e1374cd556ef456d97e218e4c16dc89f5c08c961
EthanWatkins1720 <70484753+EthanWatkins1720@users.noreply.github.com>
2023-11-06 02:37:46 -0500

    Assignment 13 test

--- public/scripts.js
+++ public/scripts.js
@@ -1,1 +1,54 @@
-console.log("Ahoy");
+const showComputers = async() => {
+    let response;
+    let computerJSON;
+
+    try {
+        response = await fetch("http://localhost:3000/api/data");
+        computerJSON = await response.json();  
+    } catch (error) {
+        console.log("Error retrieving JSON")
+    }
+
+    let computerDiv = document.getElementById("computer-list");
+
+    computerJSON.forEach((computer) => {
+        let section = document.createElement("section");
+        computerDiv.append(section);
+        
+        let h3 = document.createElement("h3");
+        section.append(h3);
+        h3.innerHTML = computer.name;
+
+        let img = document.createElement("img");
+        section.append(img);
+        img.src= computer.picture;
+
+        let color = document.createElement("p");
+        section.append(color);
+        color.innerHTML = "Color: " + computer.color;
+
+        let type = document.createElement("p");
+        section.append(type);
+        type.innerHTML = "Type: " + computer.type;
+
+        let ram = document.createElement("p");
+        section.append(ram);
+        ram.innerHTML = "RAM: " + computer.ram;
+
+        let featTitle = document.createElement("p");
+        section.append(featTitle);
+        featTitle.innerHTML = "Features: ";
+
+        let features = document.createElement("ul");
+        section.append(features);
+        for (feat in computer.features) {
+            let feature = document.createElement("li");
+            feature.innerText = computer.features[feat];
+            features.append(feature);
+        }
+    });
+};
+
+window.onload = () => {
+    showComputers();
+}
--- server.js
+++ server.js
@@ -7,8 +7,49 @@ app.get("/",(req, res) => {
 });
 
 app.get("/api/data", (req, res) => {
-    const data1 = ["1", "2", "3", "4", "5", "6"];
-    res.send(data1);
+    const computers = [];
+    computers[0] = {
+        name: "Lenovo Thinkpad X1",
+        color: "Black",
+        type: "Laptop",
+        ram: "16GB",
+        features: ["Webcam","Touchscreen","1440p Screen"],
+        picture: "../images/thinkpad.jpg"
+    };
+    computers[1] = {
+        name: "Lenovo Yoga",
+        color: "Blue",
+        type: "Laptop",
+        ram: "8GB",
+        features: ["Webcam","Touchscreen","2-in-1 Hinge"],
+        picture: "../images/yoga.png"
+    };
+    computers[2] = {
+        name: "Microsoft Surface Pro",
+        color: "Black",
+        type: "Laptop",
+        ram: "32GB",
+        features: ["Stylus","Touchscreen","2-in-1 Hinge"],
+        picture: "../images/surface.jpg"
+    };
+    computers[3] = {
+        name: "Alienware R14",
+        color: "White",
+        type: "Desktop",
+        ram: "32GB",
+        features: ["Water cooling","Discrete GPU","RGB lighting"],
+        picture: "../images/alienware.jpg"
+    };
+    computers[4] = {
+        name: "HP Pavilion",
+        color: "Black",
+        type: "Desktop",
+        ram: "8GB",
+        features: ["Silent operation","Compact design","DVD drive"],
+        picture: "../images/pavilion.jpg"
+    };
+
+    res.json(computers);
 });
 
 app.listen(3000, () => {
